c53f59cd2916c18be0b759be019d6ddef9593248
AnaytulIslamArpon <anaytularpon@gmail.com>
2020-04-11 18:03:39 +0600

    Updates

---
+++ precache-manifest.ab9c7c37aa6c8edd71f2f6ae19c7d583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08926e072bec9ca73fe4",
+    "url": "/corona-dashboard/static/css/main.53877224.chunk.css"
+  },
+  {
+    "revision": "08926e072bec9ca73fe4",
+    "url": "/corona-dashboard/static/js/main.6d16c9ff.chunk.js"
+  },
+  {
+    "revision": "189e9adabe80a96dce5d",
+    "url": "/corona-dashboard/static/js/runtime~main.6203233a.js"
+  },
+  {
+    "revision": "7003b7524644bfbb62cb",
+    "url": "/corona-dashboard/static/js/2.93c514a4.chunk.js"
+  },
+  {
+    "revision": "00ce86c1af0919cc20bc4bf953f91af1",
+    "url": "/corona-dashboard/static/media/virus.00ce86c1.svg"
+  },
+  {
+    "revision": "2f9ad274bbaff3bf7a45b82ecf8d14d3",
+    "url": "/corona-dashboard/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/corona-dashboard/precache-manifest.ab9c7c37aa6c8edd71f2f6ae19c7d583.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/corona-dashboard/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
